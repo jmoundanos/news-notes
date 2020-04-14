56a0cce4c6239822457ca185ee6babb83ca8206b
Jennifer Moundanos <jmoundanos@gmail.com>
2020-04-14 18:02:16 -0500

    add notes to database

--- controllers/articleController.js
+++ controllers/articleController.js
@@ -26,12 +26,12 @@ router.get("/scrape", function(req, res) {
         result.link = $(this)
           .children("a")
           .attr("href");
-  
+  //console.log(result);
         // Create a new Article using the `result` object built from scraping
         db.Article.create(result)
           .then(function(dbArticle) {
             // View the added result in the console
-            //console.log(dbArticle);
+            console.log(dbArticle);
           })
           .catch(function(err) {
             console.log(err);
@@ -52,18 +52,63 @@ router.get("/scrape", function(req, res) {
         res.json(err);
       });
   });
- 
-/*router.get("/articles", function(req, res) {
-    // Grab every document in the Articles collection
-    db.Article.find({})
+  router.get("/getnotes/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
       .then(function(dbArticle) {
-        // If we were able to successfully find Articles, send them back to the client
+        // If we were able to successfully find an Article with the given id, send it back to the client
         res.json(dbArticle);
       })
       .catch(function(err) {
         // If an error occurred, send it to the client
         res.json(err);
       });
-  });*/
+  });
   
+// Route for saving an article's note
+router.post("/postnotes/:id", function(req, res) {
+  // Save the new note that gets posted to the Notes collection,
+  // then find an article from the req.params.id,
+  // and update it's "note" property with the _id of the new note.
+  console.log(req.body);
+  db.Note.create(req.body)
+  .then(function(dbNote) {
+      return db.Article.findOneAndUpdate(
+          {_id: req.params.id},
+          {notes: dbNote._id},
+          {new: true }
+      );
+  })
+  // respond with the article with the note included.
+  .then(function(dbArticle) {
+      // If all Notes are successfully found, send them back to the client.
+      res.json(dbArticle);
+  })
+  .catch(function(error) {
+      // If an error occurs, send the error to the client.
+      res.json(error);
+  });
+});
+
+// Route to drop the Articles collection.
+router.delete("/delete-articles", function(req, res, next) {
+  db.Article.remove({}, function(err) {
+      if (err) {
+          console.log(err)
+      } else {
+          console.log("articles dropped!");
+      }
+  })
+  .then(function (dropnotes) {
+      db.Note.remove({}, function(err) {
+          if (err) {
+              console.log(err)
+          } else {
+              console.log("notes dropped!");
+          }
+      })
+  })
+});
 module.exports = router;
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -23,3 +23,72 @@ $("#clear-button").on("click", function(){
     }
   });
 });
+
+// Get all notes for an article.
+$(".addNote").on("click", function() {
+  // Keep the page from reloading.
+  event.preventDefault();
+  // Empty the notes from the note section
+  $(".noteArea").empty();
+  // Save the id from the button
+  var articleId = $(this).attr("data-id");
+  // Make the ajax call for the article
+  $.ajax({
+      method: "GET",
+      url: "/getnotes/" + articleId,
+      success: function () {
+          // Open the notes modal
+          $('#notesModal').modal('show');
+      }
+  })
+  // Add the note information
+  .then(function(data) {
+      // console.log(data);
+      var id = data._id;
+      // Set the title in the header.
+      $(".modal-title").html(data.title);
+      // Create a data-id attribute for the button.
+      $(".saveNote").attr("data-id", id);
+
+      // If there's already a note for the article...
+      if (data.notes) {
+          console.log(data.notes);
+          for (i=0; i<data.notes.length; i++) {
+              $(".noteArea").append(
+                  "<div class='card-body notecard' id='notecard'>" + 
+                      "<h4 class='notecardTitle' data-id='" + data.notes[i]._id + "'>" + 
+                          data.notes[i].title + 
+                      "</h4>" + 
+                      "<button type='button' class='btn btn-danger deleteNote' data-id='" + data.notes[i]._id + "'>Delete</button>" + 
+                  "</div>"
+              );
+              $(".noteArea").append(
+                  "<hr>"
+              );
+          }
+      }
+  });
+});
+// Save a Note.
+$(".saveNote").on("click", function() {
+  var articleId = $(this).attr("data-id");
+  $.ajax({
+      url: "/postnotes/" + articleId,
+      method: "POST",
+      data: {
+          // Value taken from title input
+          title: $("#titleinput").val(),
+          // Value taken from note textarea
+          body: $("#bodyinput").val()
+      }
+  })
+  .then(function(data) {
+      // Log the response
+      // console.log(data);
+      window.location.href = '/'
+      
+  });
+  // Also, remove the values entered in the input and textarea for note entry
+  $("#titleinput").val("");
+  $("#bodyinput").val("");
+});
